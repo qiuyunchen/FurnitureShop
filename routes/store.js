c5360d575eae45754e44a4f0e0e6d2505b17dffe
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-03-22 16:18:48 -0400

    added store routes

--- routes/store.js
+++ routes/store.js
@@ -4,17 +4,60 @@ const storeRouter = express.Router();
 module.exports = {storeRouter};
 
 storeRouter.post('/', (req, res, next) =>{
+    // get data from req.body & put in newStore
+    const newStore = {
+        user_id: 2, 
+        store_name: 'Mo Chairs', 
+        store_logo: 'logo url', 
+        store_desc: 'mos startup chairs'
+    };
 
+    StoreService.create(newStore)
+        .then( na =>{
+            res.json({success: 'Store created.'})
+        })
+        .catch( e =>{
+            next(e);
+        })
 })
 
-storeRouter.get('/', (req, res, next) =>{
-    
+storeRouter.get('/:store_name', (req, res, next) =>{
+    const {store_name} = req.params;
+    StoreService.read(store_name)
+        .then(store =>{
+            res.json(store);
+        })
+        .catch( e =>{
+            next(e);
+        })
 })
 
-storeRouter.put('/', (req, res, next) =>{
+storeRouter.put('/:store_id', (req, res, next) =>{
+    const {store_id} = req.params;
+    //get info from req.body
+    const updatedStore = {
+        store_id: 2, 
+        store_name: 'Mo Chairs', 
+        store_logo: 'logo url', 
+        store_desc: 'mos magnificent startup chairs'
+    };
 
+    StoreService.update(updatedStore)
+        .then(na =>{
+            res.json({success: 'Store updated.'})
+        })
+        .catch( e =>{
+            next(e);
+        })
 })
 
-storeRouter.delete('/', (req, res, next) =>{
-    
+storeRouter.delete('/:store_id', (req, res, next) =>{
+    const {store_id} = req.params;
+    StoreService.delete(store_id)
+        .then(na =>{
+            res.json({success: 'Store deleted.'})
+        })
+        .catch( e =>{
+            next(e);
+        })
 })
